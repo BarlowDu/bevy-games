b244ee87aaae3b92b379924a4ba658453c661aa9
10004602 <dutc@bthome.com>
2026-05-16 15:09:11 +0800

    添加字体

--- examples/matrix4.rs
+++ examples/matrix4.rs
@@ -1,14 +1,10 @@
-use bevy::{
-    prelude::*,
-    text::{FontSmoothing},
-};
+use bevy::{prelude::*, text::FontSmoothing};
 
 use rand::{self, RngExt};
 
-const ACCELERATION:f32=49.0;
+const ACCELERATION: f32 = 49.0;
 
 fn main() {
-
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
@@ -29,7 +25,7 @@ struct Fall {
     speed: f32,
 }
 
-fn setup(mut commands: Commands,asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/msyh.ttc");
     let text_font = TextFont {
         font: font.clone(),
@@ -45,53 +41,66 @@ fn setup(mut commands: Commands,asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
     let mut rng = rand::rng();
     for i in 0..=300 {
-        let parent=commands.spawn((
-             Sprite::from_color(Color::NONE, Vec2::new(10.,500.)),
-             Transform::from_translation(Vec3::new(-rng.random_range(0.9..1.5)*1000. + (i * 25) as f32, 500., 0.)),
-             Fall {
-                speed: get_init_speed()*2.0,
-            },
-            
-            //children![a]
-        )).id();
+        let parent = commands
+            .spawn((
+                Sprite::from_color(Color::NONE, Vec2::new(10., 500.)),
+                Transform::from_translation(Vec3::new(
+                    -rng.random_range(0.9..1.5) * 1000. + (i * 25) as f32,
+                    500.,
+                    0.,
+                )),
+                Fall {
+                    speed: get_init_speed() * 2.0,
+                },
+                //children![a]
+            ))
+            .id();
         //let mut pentity=commands.entity(parent);
-        let ch=get_fall_char(i);
-        for i in 0..=50{
-             let child=commands.spawn((
-                Text2d::new(&ch),
-                text_font.clone().with_font_smoothing(FontSmoothing::AntiAliased),
-                TextColor::from(Color::srgba(0.,1.,0.,1.0-(i as f32*0.02))),
-                Transform::from_translation(Vec3::new(0.,(-250+(10*i)) as f32,10.)),
-             )).id();
-             commands.entity(parent).add_child(child);
-        }
-         /*for item in a{
-            let child=commands.spawn(item).id();
+        let ch = get_fall_char(i);
+        for i in 0..=50 {
+            let child = commands
+                .spawn((
+                    Text2d::new(&ch),
+                    text_font
+                        .clone()
+                        .with_font_smoothing(FontSmoothing::AntiAliased),
+                    TextColor::from(Color::srgba(0., 1., 0., 1.0 - (i as f32 * 0.02))),
+                    Transform::from_translation(Vec3::new(0., (-250 + (10 * i)) as f32, 10.)),
+                ))
+                .id();
             commands.entity(parent).add_child(child);
-         }*/
+        }
+        /*for item in a{
+           let child=commands.spawn(item).id();
+           commands.entity(parent).add_child(child);
+        }*/
     }
 }
 
-fn animate_fall(mut query: Query<(&mut Transform, &mut Fall), With<Sprite>>,time:Res<Time>) {
+fn animate_fall(mut query: Query<(&mut Transform, &mut Fall), With<Sprite>>, time: Res<Time>) {
     // Consider changing font-size instead of scaling the transform. Scaling a Text2D will scale the
     // rendered quad, resulting in a pixellated look.
     for (mut transform, mut f) in &mut query {
         if transform.translation.y < -500. {
             transform.translation.y = 500.;
-            f.speed=get_init_speed();
+            f.speed = get_init_speed();
         }
-        f.speed=f.speed+ACCELERATION*time.delta_secs();
+        f.speed = f.speed + ACCELERATION * time.delta_secs();
         transform.translation.y -= f.speed;
     }
 }
 
-fn get_fall_char(index:i32)->String{
-    let charset=vec!["甲","乙","丙","丁","戊","己","庚","辛","壬","癸","子","丑","寅","卯","辰","巳","午","未","申","酉","戌","亥"];
-    let i:usize=(index.abs() as usize)%charset.len();
+fn get_fall_char(index: i32) -> String {
+    let charset = vec![
+        "Α", "α", "Β", "β", "Γ", "γ", "Δ", "δ", "Ε", "ε,", "Ζ", "ζ", "Η", "η", "Θ", "θ", "Ι", "ι",
+        "Κ", "κ", "Λ", "λ", "Μ", "μ", "Ν", "ν", "Ξ", "ξ", "ϵ", "ς", "Ο", "ο", "Π", "π", "Ρ", "ρ",
+        "Σ", "σ,", "Τ", "τ", "Υ", "υ", "Φ", "ϕ", "Χ", "χ", "Ψ", "ψ", "Ω", "ω",
+    ];
+    let i: usize = (index.abs() as usize) % charset.len();
     return charset[i].to_string();
 }
 
-fn get_init_speed()->f32{
-     let mut rng = rand::rng();
-     return rng.random_range(20.0..40.0);
+fn get_init_speed() -> f32 {
+    let mut rng = rand::rng();
+    return rng.random_range(20.0..40.0);
 }
