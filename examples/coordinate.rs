056d5863e313044e37c286c1b71646f7b687365b
10004602 <dutc@bthome.com>
2026-05-16 21:19:33 +0800

    优化坐标

--- examples/coordinate.rs
+++ examples/coordinate.rs
@@ -1,4 +1,4 @@
-use bevy::{color::palettes::css::*, prelude::*};
+use bevy::{color::palettes::{css::*, tailwind::RED_950}, prelude::*};
 use std::f32::consts::{FRAC_PI_2, FRAC_PI_4, PI, TAU};
 
 fn main() {
@@ -11,44 +11,46 @@ fn main() {
 }
 
 const RATE: f32 = 15.0;
-const INTERVAL: f32 = 26.0;
+const INTERVAL: f32 = 24.0;
 
-fn draw_function(mut gizmos: Gizmos, time: Res<Time>) {
-    let domain = Interval::EVERYWHERE;
-    let curve = FunctionCurve::new(domain, |t| Vec2::new(t, (t / RATE).powf(2.0) * RATE));
+struct FUNCTION {
+    func: Box<dyn Fn(f32) -> f32>,
+    color: Srgba,
+}
 
-    let times_and_colors = (-100..=100)
-        .map(|n| (n as f32) / INTERVAL * RATE)
-        .map(|t| (t, RED));
-    gizmos.curve_gradient_2d(curve, times_and_colors);
-
-    let curve = FunctionCurve::new(domain, |t| Vec2::new(t, (t / RATE).powf(3.0) * RATE));
-
-    let times_and_colors = (-100..=100)
-        .map(|n| (n as f32) / INTERVAL * RATE)
-        .map(|t| (t, YELLOW));
-    gizmos.curve_gradient_2d(curve, times_and_colors);
-
-    let curve = FunctionCurve::new(domain, |t| Vec2::new(t, (t / RATE).sin() * RATE));
-
-    let times_and_colors = (-100..=100)
-        .map(|n| (n as f32) / INTERVAL * RATE)
-        .map(|t| (t, BLUE));
-    gizmos.curve_gradient_2d(curve, times_and_colors);
-
-        let curve = FunctionCurve::new(domain, |t| Vec2::new(t, (t / RATE).cos() * RATE));
-
-    let times_and_colors = (-100..=100)
-        .map(|n| (n as f32) / INTERVAL * RATE)
-        .map(|t| (t, PURPLE));
-    gizmos.curve_gradient_2d(curve, times_and_colors);
+impl FUNCTION {
+    fn new(func: impl Fn(f32) -> f32 + 'static, color: Srgba) -> Self {
+        FUNCTION {
+            func: Box::new(func),
+            color,
+        }
+    }
     
-        let curve = FunctionCurve::new(domain, |t| Vec2::new(t, (t / RATE).powf(t / RATE) * RATE));
+}
+fn draw_function(mut gizmos: Gizmos,) {
+    let domain = Interval::EVERYWHERE;
+    let resolution=100;    
+    let convert=|n:f32|( INTERVAL * RATE/(resolution as f32))*n;
+    let function_vec=vec![
+        FUNCTION::new(|t| t, RED),
+        FUNCTION::new(|t| t.powf(2.0), YELLOW),
+        FUNCTION::new(|t| t.powf(3.0), GREEN),
 
-    let times_and_colors = (-100..=100)
-        .map(|n| (n as f32) / INTERVAL * RATE)
-        .map(|t| (t, GREEN));
-    gizmos.curve_gradient_2d(curve, times_and_colors);
+        
+        FUNCTION::new(|t| t.sin(), BLUE),        
+        FUNCTION::new(|t| t.cos(), PURPLE),     
+        FUNCTION::new(|t| t.cos()+t.sin(), BLACK),
+    ];
+
+    for item in function_vec {
+        let curve = FunctionCurve::new(domain, |t| Vec2::new(t, (item.func)(t/RATE)*RATE));
+        let times_and_colors = (-resolution..=resolution)
+            .map(|n| convert(n as f32))
+            .map(|t| (t, item.color));
+        gizmos.curve_gradient_2d(curve, times_and_colors);
+    }
+
+
 }
 fn setup(
     mut commands: Commands,
@@ -57,7 +59,7 @@ fn setup(
 ) {
     commands.spawn(Camera2d);
 
-    let half_length = INTERVAL * RATE + RATE / 2.0;
+    let half_length = INTERVAL * RATE + RATE * 0.8;
     commands.spawn((
         Mesh2d(meshes.add(Segment2d::new(
             Vec2::new(-half_length, 0.0),
@@ -75,10 +77,11 @@ fn setup(
         Transform::from_translation(Vec3::ZERO),
     ));
 
+    let point_height = INTERVAL * 0.3;
     for i in -(INTERVAL as i32)..=(INTERVAL as i32) {
         commands.spawn((
             Mesh2d(meshes.add(Segment2d::new(
-                Vec2::new((i as f32) * RATE, 10.0),
+                Vec2::new((i as f32) * RATE, point_height),
                 Vec2::new((i as f32) * RATE, 0.0),
             ))),
             MeshMaterial2d(materials.add(Color::from(BLACK))),
@@ -87,15 +90,14 @@ fn setup(
 
         commands.spawn((
             Mesh2d(meshes.add(Segment2d::new(
-                Vec2::new(10.0, (i as f32) * RATE),
+                Vec2::new(point_height, (i as f32) * RATE),
                 Vec2::new(0.0, (i as f32) * RATE),
             ))),
             MeshMaterial2d(materials.add(Color::from(BLACK))),
             Transform::from_translation(Vec3::ZERO),
         ));
     }
-    let arrow_length = INTERVAL*1.5;
-    let arrow_sin = arrow_length * (FRAC_PI_4.sin());
+    let arrow_sin = point_height * (FRAC_PI_4.sin());
 
     commands.spawn((
         Mesh2d(meshes.add(Segment2d::new(
